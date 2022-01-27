55aa6a296548c50fa5af16dcbaa96a2687f23da8
João Silva <jloucaosilva@users.noreply.github.com>
2022-01-27 12:11:16 +0000

    Adding example
    Console application with json settings

---
+++ JSonSettingsConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace JSonSettingsConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Setup Host
+        var host = CreateDefaultBuilder().Build();
+
+        // Invoke Worker
+        using (IServiceScope serviceScope = host.Services.CreateScope())
+        {
+            IServiceProvider provider = serviceScope.ServiceProvider;
+            var running = provider.GetRequiredService<Running>();
+
+            running.Run();
+        }
+
+        host.Run();
+    }
+
+    static IHostBuilder CreateDefaultBuilder()
+    {
+        return Host.CreateDefaultBuilder()
+            .ConfigureAppConfiguration(app =>
+            {
+                app.AddJsonFile("appsettings.json");
+            })
+            .ConfigureServices(services =>
+            {
+                services.AddSingleton<Running>();
+            });
+    }
+}
---
+++ JSonSettingsConsoleApp/Running.cs
@@ -0,0 +1,14 @@
+﻿namespace JSonSettingsConsoleApp;
+
+public class Running
+{
+    public void Run()
+    {
+        Console.WriteLine("Input your name pls: ");
+        var x = Console.ReadLine();
+
+        Console.WriteLine($"Hello {x}");
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+    }
+}
